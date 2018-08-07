28262674c7629d7872809da1af591af73c9b4abc
luscasos <38993978+luscasos@users.noreply.github.com>
2018-08-07 20:35:37 -0300

    Bluetooth request
    solicitando ativação do bluetooth assim que é iniciado o aplicativo

--- app/src/main/java/com/example/lucas/plotterbluetooth/MainActivity.java
+++ app/src/main/java/com/example/lucas/plotterbluetooth/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lucas.plotterbluetooth;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +12,25 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int bluetoothR0equest = 1;
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inicialização do bluetooth
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "Não há suporte a Bluetooth", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Finalizando", Toast.LENGTH_SHORT).show();
+            finish();
+        }   else if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, bluetoothR0equest);
+        }
     }
 
     public void manual(){
--- app/src/main/java/com/example/lucas/plotterbluetooth/Manual.java
+++ app/src/main/java/com/example/lucas/plotterbluetooth/Manual.java
@@ -1,7 +1,12 @@
 package com.example.lucas.plotterbluetooth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Manual extends AppCompatActivity {
 
@@ -9,6 +14,34 @@ public class Manual extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.primeiroBotao:
+                manual();
+                return true;
+            case R.id.segundoBotao:
+                Toast.makeText(this, "segundo botão", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void manual(){
+
+        Intent intent = new Intent(this, Manual.class);
+        startActivity(intent);
     }
 
 }
