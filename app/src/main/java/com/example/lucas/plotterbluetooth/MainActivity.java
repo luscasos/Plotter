5d9cfa9ffe4e9ee1ef0bdae8a60145eb3a9d3eb5
luscasos <38993978+luscasos@users.noreply.github.com>
2018-08-07 20:20:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/plotterbluetooth/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.lucas.plotterbluetooth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void manual(){
+
+        Intent intent = new Intent(this, Manual.class);
+        startActivity(intent);
+    }
+    public void manual(View view){
+
+        Intent intent = new Intent(this, Manual.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.primeiroBotao:
+                manual();
+                return true;
+            case R.id.segundoBotao:
+                Toast.makeText(this, "segundo botão", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
